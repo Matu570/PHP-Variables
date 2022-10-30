baf0e47c4965046880ccdd5128c027f8ab0b1a8e
Matias Bueno <matiasbueno@hotmail.com.ar>
2022-10-30 00:31:56 -0300

    Creation and modification of variables

---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- Creation of variable -->
+<!-- A variable is a information container -->
+<?php
+    $my_first_variable='Hello Wold from a variable';
+    $numero=44;
+    $verdadero=true;    
+    $numero=77;
+    echo '<h1>'.$my_first_variable.'</h1>';
+    echo $numero.'<br>';
+    $numero=120;
+    echo $numero;
+?>
